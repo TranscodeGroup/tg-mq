e712fd6d60c484ea20090996050d646e97a2a9e0
xiaorongnie <330811792@qq.com>
2020-12-16 16:58:54 +0800

    *消息头增加扩展位,流水号不限制65535

--- src/main/java/com/tgroup/mq/MqHeader.java
+++ src/main/java/com/tgroup/mq/MqHeader.java
@@ -1,9 +1,15 @@
 package com.tgroup.mq;
 
+import java.util.HashMap;
+
+import lombok.Data;
+
 /**
  * REDIS订阅消息头
+ * 
  * @author YI
  */
+@Data
 public class MqHeader {
     /**
      * 指令
@@ -18,57 +24,16 @@ public class MqHeader {
      */
     private String target;
     /**
-     * 流水号 从0开始 0-65535
+     * 流水号 从0开始 0-MaxInt
      */
     private int serial;
     /**
      * 发送时间
      */
     private long time;
-    
-    public String getCmd() {
-        return cmd;
-    }
-    
-    public void setCmd(String cmd) {
-        this.cmd = cmd;
-    }
-    
-    public String getSender() {
-        return sender;
-    }
-    
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
-    
-    public String getTarget() {
-        return target;
-    }
-    
-    public void setTarget(String target) {
-        this.target = target;
-    }
-    
-    public int getSerial() {
-        return serial;
-    }
-    
-    public void setSerial(int serial) {
-        this.serial = serial;
-    }
-    
-    public long getTime() {
-        return time;
-    }
-    
-    public void setTime(long time) {
-        this.time = time;
-    }
-    
-    @Override
-    public String toString() {
-        return "Header [cmd=" + cmd + ", sender=" + sender + ", target=" + target + ", serial=" + serial + ", time="
-                + time + "]";
-    }
+
+    /***
+     * 扩展信息
+     */
+    private HashMap<String, Object> properties;
 }
---
+++ src/main/java/com/tgroup/mq/constant/Jtt808Protocol.java
@@ -0,0 +1,36 @@
+package com.tgroup.mq.constant;
+
+/**
+ * 连接类型
+ * 
+ * @author eason
+ * @date 2020/12/16
+ */
+public enum Jtt808Protocol {
+
+    /**
+     * 808,1078设备
+     */
+    JTT_808,
+    /**
+     * 单独调度设备
+     */
+    JTT_808_ITS,
+    /***
+     * 单独视频设备
+     */
+    JTT_808_DVR;
+
+    public static final String JTT808_PROTOCOL_KEY = "jtt808";
+
+    public static Jtt808Protocol toProtocol(String protocol) {
+        switch (protocol) {
+            case "ITS":
+                return JTT_808_ITS;
+            case "DVR":
+                return JTT_808_DVR;
+            default:
+                return JTT_808;
+        }
+    }
+}
