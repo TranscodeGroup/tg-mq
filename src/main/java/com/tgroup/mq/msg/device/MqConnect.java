361468aa3915d0c28d8d26cbdc0a97cecf2f199e
xiaorongnie <330811792@qq.com>
2020-12-17 11:23:55 +0800

    终端连接增加断开备注

--- src/main/java/com/tgroup/mq/msg/device/MqConnect.java
+++ src/main/java/com/tgroup/mq/msg/device/MqConnect.java
@@ -2,55 +2,37 @@ package com.tgroup.mq.msg.device;
 
 import com.tgroup.mq.msg.MqDeviceData;
 
-/**
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/***
  * 终端连接与断开
+ * 
+ * @author eason
+ * @date 2020/12/17
  */
+@Data
+@EqualsAndHashCode(callSuper = false)
 public class MqConnect extends MqDeviceData {
-    // 企业
+    /**
+     * 终端生产企业
+     */
     private String company;
-    // 0建立连接 1断开连接
+    /***
+     * 0建立连接 1断开连接
+     */
     private Integer flag;
-    // 连接时间
+    /**
+     * 连接时间
+     */
     private Long connecttime;
-    // 远程地址
+    /**
+     * 远程地址
+     */
     private String address;
-
-    @Override
-    public String toString() {
-        return "GpsConnect [devid=" + getImei() + ",company=" + company + ",flag=" + flag + ",time=" + getEventTime()
-                + ",connecttime=" + connecttime + "]";
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-    public Integer getFlag() {
-        return flag;
-    }
-
-    public void setFlag(Integer flag) {
-        this.flag = flag;
-    }
-
-    public Long getConnecttime() {
-        return connecttime;
-    }
-
-    public void setConnecttime(Long connecttime) {
-        this.connecttime = connecttime;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
+    /**
+     * 终端断开原因备注
+     */
+    private String remark;
 
 }
