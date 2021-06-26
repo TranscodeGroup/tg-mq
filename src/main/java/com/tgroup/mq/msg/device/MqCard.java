dc33d784144b0d496d54c9106905ae2ce9b09055
xiaorongnie <330811792@qq.com>
2021-06-26 11:54:29 +0800

    v1.0.32刷卡签到归类

--- src/main/java/com/tgroup/mq/msg/device/MqCard.java
+++ src/main/java/com/tgroup/mq/msg/device/MqCard.java
@@ -21,69 +21,60 @@ import lombok.ToString;
 public class MqCard extends MqDeviceData {
 
     /**
-     * 0签退 1签到
+     * 泰国定制部分
      */
+    /** 0签退 1签到 */
     private Short checkin;
-    /**
-     * 0非法 1合法
-     */
+
+    /** 0非法 1合法 */
     private Short valid;
-    /**
-     * 考勤方式 0默认 1员工卡 2手工输入
-     */
-    private Short source;
-    /**
-     * 刷卡完整数据
-     */
+
+    /** 磁条卡刷卡完整数据 */
     private String rfid;
-    /**
-     * 简易卡ID
-     */
-    private String cardId;
 
     /**
-     * 状态位
+     * 808协议部分
      */
+    /** 简易卡ID或者员工编号 */
+    private String cardId;
+
+    /** 考勤类型 1上班 2下班 3签到 4签退 5例检 **/
+    private Short type;
+
+    /** 考勤方式 1员工卡考勤 2输工号考勤 自定义200=GPS附加, 210透传 **/
+    private Short origin;
+
+    /***
+     * 扩展GPS
+     */
+
+    /** 状态位 */
     @JsonProperty("s")
     private Integer state;
 
-    /**
-     * 报警位
-     */
+    /** 报警位 */
     @JsonProperty("w")
     private Integer warning;
 
-    /**
-     * 速度
-     */
+    /** 速度 */
     @JsonProperty("v")
     private Integer speed;
 
-    /**
-     * 海拔
-     */
+    /** 海拔 */
     @JsonProperty("h")
     private Integer height;
 
-    /**
-     * 经度
-     */
+    /** 经度 */
     private Double lng;
 
-    /**
-     * 纬度
-     */
+    /** 纬度 */
     private Double lat;
 
-    /**
-     * 方位角
-     */
+    /** 方位角 */
     @JsonProperty("a")
     private Integer azimuth;
 
-    /**
-     * 当前累计里程
-     */
+    /** 当前累计里程 */
     @JsonProperty("m")
     private Double mileage;
 
