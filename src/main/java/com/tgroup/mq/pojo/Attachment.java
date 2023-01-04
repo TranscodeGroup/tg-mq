6f8bbe306e70bdefd5efb7b1958965145a0070f1
xiaorongnie <xiaorongnie@transcodegroup.com>
2023-01-04 09:57:25 +0800

    v22.12.30

--- src/main/java/com/tgroup/mq/pojo/Attachment.java
+++ src/main/java/com/tgroup/mq/pojo/Attachment.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * 
